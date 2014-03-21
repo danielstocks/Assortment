cc2fc9f98f92913503a13a4367dd3e7675456633
Daniel Stocks <daniel@webcloud.se>
2014-03-21 17:03:06 +0100

    *gulp*

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+
+gulp.task('minify-css', function() {
+  gulp.src(['css/reset.css','css/*.css'])
+    .pipe(minifyCSS(opts))
+    .pipe(concat('app.min.css'))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('compress', function() {
+  gulp.src([
+      'js/vendor/*.js',
+      'js/util/*.js',
+      'js/sorting/*.js',
+      'js/app.js',
+      'js/chart.js',
+      'js/sound.js',
+      'js/crazyMode.js',
+      'js/controls.js',
+      'js/animation.js'
+    ])
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('dist'))
+});
