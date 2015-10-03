5be4d926051447cae5cb24fad0f9432449dd5811
Daniel Stocks <daniel@webcloud.se>
2015-10-03 11:53:13 +0200

    Fixed broken sound. closes #1

--- js/sound.js
+++ js/sound.js
@@ -2,77 +2,57 @@
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext
 
-var minFreq = 150;
-var maxFreq = 500;
-var context = new AudioContext();
 var sound = {}
+var minFreq = 175;
+var maxFreq = 600;
+var context = new AudioContext();
+var volume = 0.5;
 
-var pannerNode = context.createPanner();
-pannerNode.setPosition(10, 5, 0);
+function playOscillator(freq, type, pan) {
+    var node = context.createGain();
+    var panner = context.createPanner();
+    var oscillator = context.createOscillator();
+    if(pan) {
+      panner.setPosition(10, 5, 0);
+    } else {
+      panner.setPosition(-10, -5, 0);
+    }
+    node.connect(panner);
+    panner.connect(context.destination);
+    node.gain.value = 0.0;
+    oscillator.type = type;
+    oscillator.frequency.value = freq;
+    oscillator.connect(node);
+    oscillator.start(context.currentTime);
+    node.gain.value = volume;
+    oscillator.stop(context.currentTime + 0.5);
 
-var pannerNode2 = context.createPanner();
-pannerNode2.setPosition(-10, -5, 0);
-
-var gainNode = context.createGain();
-var gainNode2 = context.createGain();
-
-gainNode.connect(pannerNode);
-gainNode.gain.value = 0.0;
-
-gainNode2.connect(pannerNode2);
-gainNode2.gain.value = 0.0;
-
-pannerNode.connect(context.destination);
-pannerNode2.connect(context.destination);
-
-function osc(type, pan) {
-  var oscillator = context.createOscillator();
-  oscillator.type = type;
-  if(pan == "left") {
-    oscillator.connect(gainNode);
-  } else {
-    oscillator.connect(gainNode2);
-  }
-  oscillator.start(0);
-  oscillator.frequency.value = 0;
-  return oscillator
+    // The sound should last for 250ms
+    setTimeout(function() {
+      node.gain.value = 0;
+    }, 300);
+    return oscillator;
 }
+
 
 sound.draw = function(i) {
   var step = ((maxFreq - minFreq) / currentList.length);
   var v1 = (swaps[i][1] * step) + minFreq;
   var v2 = (swaps[i][2] * step) + minFreq;
-
   if(isFinite(v1)) {
-    var o1 = osc(1, "left");
-    o1.frequency.value = v1
+    playOscillator(v1, "triangle", true);
   }
   if(isFinite(v2)) {
-    var o2 = osc(2, "right");
-    o2.frequency.value = v2;
+    playOscillator(v2, "square");
   }
-  (function(o1, o2) {
-    window.setTimeout(function() {
-      if(o1) {
-      o1.stop(0);
-      o1.disconnect();
-      }
-      if(o2) {
-        o2.stop(0);
-        o2.disconnect();
-      }
-    }, 50);
-  })(o1,o2);
 }
 
 sound.mute = function() {
-  gainNode.gain.value = 0.0;
-  gainNode2.gain.value = 0.0;
+  volume = 0;
 }
 
 sound.unmute = function() {
-  gainNode.gain.value = 0.2;
-  gainNode2.gain.value = 0.2;
+  volume = 0.5;
 }
 
 state.on("sound-mute", sound.mute);
